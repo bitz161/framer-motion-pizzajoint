bad933584ad826c9430148dec28e591db7f07273
bitz161 <bitzgarcia1996@gmail.com>
2022-08-03 21:47:07 +0800

    Updates

---
+++ precache-manifest.1f048f31ddd4e369c89f5c680d1242e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a08f2542c535d35bd65d2ae9d583d63",
+    "url": "/framer-motion-pizzajoint/index.html"
+  },
+  {
+    "revision": "5010385405597f3deea9",
+    "url": "/framer-motion-pizzajoint/static/css/main.f087abb0.chunk.css"
+  },
+  {
+    "revision": "26e0e9535c9259dec0ae",
+    "url": "/framer-motion-pizzajoint/static/js/2.ce8d853a.chunk.js"
+  },
+  {
+    "revision": "216e537bacc64f9dbaabd26649a9ab95",
+    "url": "/framer-motion-pizzajoint/static/js/2.ce8d853a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5010385405597f3deea9",
+    "url": "/framer-motion-pizzajoint/static/js/main.0b52786c.chunk.js"
+  },
+  {
+    "revision": "ae3230abe41b77d947bf",
+    "url": "/framer-motion-pizzajoint/static/js/runtime-main.acf1e545.js"
+  }
+]);
